8438c5546a5a6c433bd1610d2fcfeb3af097e0af
macsoares1275 <macsoares1275@gmail.com>
2025-04-01 01:41:12 +0000

    Adaptação da troa de card ao escolher uma nota da avaliação

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,15 @@ import iconStar from "./assets/icon-star.svg";
 import { useState } from "react"
 
 export function App(){
-  const [nota, setNotaAvaliacao] = useState(0)
+  const [notaAvaliacao, setNotaAvaliacao] = useState(0)
 
   function handleMudarNotaAvaliacao(nota){
     setNotaAvaliacao(nota)
   }
 
   return (
-    
-    <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+    notaAvaliacao === 0 ? (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
       <div className="bg-dark-blue w-fit p-4 rounded-full mb-4">
         <img src={iconStar} alt="icon star" />
       </div>
@@ -29,6 +29,11 @@ export function App(){
 
         <button className="bg-orange w-full uppercase tracking-1 font-bold rounded-3xl text-sm py-3">Submit</button>
       
-    </div>
+      </div>
+    ) : (
+      <div className="bg-gradient-dark text-white mx-6 p-6 rounded-2xl font-overpass">
+        <p>{notaAvaliacao}</p>
+      </div>
+    )
   )
 }
